be8840e1b123706b68272fcf2e085d76d8d49bd6
Shakil Khan <60468711+shakilkhandev@users.noreply.github.com>
2024-12-05 20:31:46 +0600

    Create operations_with_other_types.py

---
+++ operations_with_other_types.py
@@ -0,0 +1,11 @@
+savings = 100
+new_savings = 40
+
+# Calculate total_savings using savings and new_savings
+
+total_savings = savings + new_savings 
+
+print(total_savings)
+
+# Print the type of total_savings
+print(type(total_savings))
