bfea61f2f1d5c027df91d437378dd7f46e9c7e61
Shakil Khan <60468711+shakilkhandev@users.noreply.github.com>
2024-12-05 20:43:47 +0600

    Create replace&extend_list_element.py

---
+++ replace&extend_list_element.py
@@ -0,0 +1,20 @@
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Correct the bathroom area
+
+areas[-1]=10.50
+
+# Change "living room" to "chill zone"
+
+areas[-6] = "chill zone"
+
+# Create the areas list and make some changes
+areasnew = ["hallway", 11.25, "kitchen", 18.0, "chill zone", 20.0,
+         "bedroom", 10.75, "bathroom", 10.50]
+
+# Add poolhouse data to areas, new list is areas_1
+areas_1 = areasnew + ["poolhouse", 24.5]
+
+# Add garage data to areas_1, new list is areas_2
+areas_2 = areas_1 + ["garage",15.45]
