93bc356d6d60793cf9a734dcc2af08ca8b497dd1
Shakil Khan <60468711+shakilkhandev@users.noreply.github.com>
2024-12-05 20:28:12 +0600

    Create pythonBasic.py

---
+++ pythonBasic.py
@@ -0,0 +1,26 @@
+print(5 / 8)
+
+
+
+# Addition
+
+print(4+5)
+# Subtraction
+print(5-5)
+
+# Multiplication
+print(3 * 5)
+
+# Division
+print(10/2)
+
+
+
+# Create a variable savings
+savings = 100
+
+# Print out savings
+
+print (savings)
+
+
