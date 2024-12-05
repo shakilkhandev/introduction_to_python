c036bd7d02cf2575f61ee79e305e7552b98393a2
Shakil Khan <60468711+shakilkhandev@users.noreply.github.com>
2024-12-05 20:47:22 +0600

    Create delete_item_from_a_list.py

---
+++ delete_item_from_a_list.py
@@ -0,0 +1,19 @@
+areas = ["hallway", 11.25, "kitchen", 18.0,
+        "chill zone", 20.0, "bedroom", 10.75,
+         "bathroom", 10.50, "poolhouse", 24.5,
+         "garage", 15.45]
+
+# Delete the poolhouse items from the list 
+
+
+
+del areas[10]
+
+# delete the value 24.5 __now this is on 10th index so ...
+
+del areas[10]
+
+
+# Print the updated list
+
+print(areas)
